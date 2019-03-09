b3d0c31974275621a93c4f4d68ad150b9b9fa945
Henrik Drefs <henrik.drefs@outlook.de>
2019-03-09 13:40:22 +0100

    check jq install

--- functions/test/gcloud-ocr.sh
+++ functions/test/gcloud-ocr.sh
@@ -1,2 +1,4 @@
 
+command -v jq >/dev/null 2>&1 || { echo >&2 "'jq' JSON query tool is required but it's not installed: Visit https://stedolan.github.io/jq/"; exit 1; }
+
 gcloud ml vision detect-text $1 | jq -r '.responses[0].fullTextAnnotation.text'
