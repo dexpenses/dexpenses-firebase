1a0fbe6412d10e4ca52474c9f006d79607b8bc8d
Henrik Drefs <henrik.drefs@outlook.de>
2019-04-13 00:30:34 +0200

    test unmapped place type

--- functions/src/tagging/TaggingEngine.spec.ts
+++ functions/src/tagging/TaggingEngine.spec.ts
@@ -1,6 +1,17 @@
 import TaggingEngine from './TaggingEngine';
 
 describe('TaggingEngine', () => {
+  it('should not fail if place type not found', () => {
+    const engine = new TaggingEngine([]);
+    expect(
+      engine.tag({
+        place: {
+          types: ['non_existing_place_type'],
+        } as any,
+      })
+    ).toEqual([]);
+  });
+
   it('should infer tags from single place type', () => {
     const engine = new TaggingEngine([]);
     expect(
