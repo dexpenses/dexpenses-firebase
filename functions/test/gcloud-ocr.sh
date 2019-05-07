64adb1c3ebf88eb4c9c53ef4eaf47b21a61f9430
Henrik Drefs <henrik.drefs@outlook.de>
2019-05-07 22:48:53 +0200

    default ext JPEG, mkdir target

--- functions/test/gcloud-ocr.sh
+++ functions/test/gcloud-ocr.sh
@@ -1,5 +1,7 @@
 
 command -v jq >/dev/null 2>&1 || { echo >&2 "'jq' JSON query tool is required but it's not installed: Visit https://stedolan.github.io/jq/"; exit 1; }
 
-ext=${2:-png}
+mkdir -p $(dirname data/$1)
+
+ext=${2:-JPEG}
 gcloud ml vision detect-text gs://dexpenses-207219-test-images/$1.$ext | jq -r '.responses[0].fullTextAnnotation.text' > data/$1.txt
