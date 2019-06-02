f07859d798e4673f52b777711f7eb489fc964dc0
Henrik Drefs <henrik.drefs@outlook.de>
2019-06-02 21:34:56 +0200

    fully tested

--- functions/test/receipt-state-updater.offline.spec.ts
+++ functions/test/receipt-state-updater.offline.spec.ts
@@ -1,39 +1,81 @@
 import * as firebaseFunctionsTest from 'firebase-functions-test';
 import * as admin from 'firebase-admin';
 import { updateFn, firestore } from './firebase-stubs';
+import * as extractor from '../src/extract/pipeline';
 
 const test = firebaseFunctionsTest();
 
 jest.spyOn(admin, 'initializeApp');
 
-//jest.spyOn(admin, 'firestore' as any, 'get').mockReturnValue(() => firestore);
-// jest.mock('firebase-admin', () => require('./firebase-mock-sdk').mocksdk);
-
 const receiptStateUpdater = test.wrap(require('../src').receiptStateUpdater);
 
-beforeEach(() => {
-  updateFn.mockClear();
-});
-
 describe('Receipt state updater cloud function (offline)', () => {
-  it('should not do anything if result data is absent', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    /**
+     * test.firestore.makeDocumentSnapshot and test.makeChange
+     * require actual firestore instance
+     */
+    jest.spyOn(admin, 'firestore' as any, 'get').mockRestore();
+    jest.spyOn(extractor, 'isReady').mockRestore();
+  });
+
+  it.each([
+    ['result is absent', {}, { downloadURL: 'foo.me' }],
+    ['result data is absent', {}, { result: { state: 'error' } }],
+    [
+      'result state is not ready and not partial',
+      {},
+      { result: { state: 'error', data: {} } },
+    ],
+    [
+      'result state did not change',
+      { result: { data: {}, state: 'partial' } },
+      { result: { data: {}, state: 'partial' } },
+    ],
+  ] as any[])('should not do anything if %s', async (name, before, after) => {
+    await changeEvent({
+      before,
+      after,
+    });
+    expect(updateFn).not.toHaveBeenCalled();
+  });
+
+  it('should update state if receipt becomes ready', async () => {
+    jest.spyOn(extractor, 'isReady').mockReturnValue(true);
+
+    await changeEvent({
+      before: {},
+      after: { result: { data: {}, state: 'partial' } },
+    });
+    expect(updateFn).toHaveBeenCalledWith({ 'result.state': 'ready' });
+  });
+
+  it('should update state if receipt becomes partial', async () => {
+    jest.spyOn(extractor, 'isReady').mockReturnValue(false);
+
+    await changeEvent({
+      before: {},
+      after: { result: { data: {}, state: 'ready' } },
+    });
+
+    expect(updateFn).toHaveBeenCalledWith({ 'result.state': 'partial' });
+  });
+
+  async function changeEvent(e: { before: any; after: any }) {
     const refPath = 'receiptsByUser/testUserId/receipts/test-receipt.jpg';
 
-    const before = test.firestore.makeDocumentSnapshot({}, refPath);
-    const after = test.firestore.makeDocumentSnapshot(
-      {
-        downloadURL: 'bar',
-      },
-      refPath
-    );
+    const before = test.firestore.makeDocumentSnapshot(e.before, refPath);
+    const after = test.firestore.makeDocumentSnapshot(e.after, refPath);
     const change = test.makeChange(before, after);
-    Object.defineProperty(admin, 'firestore', { get: () => firestore });
+    jest
+      .spyOn(admin, 'firestore' as any, 'get')
+      .mockReturnValue(() => firestore);
     await receiptStateUpdater(change, {
       params: {
         userId: 'testUserId',
         receiptId: 'test-receipt.jpg',
       },
     });
-    expect(updateFn).not.toHaveBeenCalled();
-  });
+  }
 });
