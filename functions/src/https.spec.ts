5aca72c6f487f19d8f99934c4ce8c4d5103c6497
Henrik Drefs <henrik.drefs@outlook.de>
2019-07-10 21:15:32 +0200

    authorized callable helper

--- functions/src/https.spec.ts
+++ functions/src/https.spec.ts
@@ -1,10 +1,13 @@
 import * as fbTest from 'firebase-functions-test';
 import * as admin from 'firebase-admin';
 import * as https from './https';
+import { anyOf } from './https';
+import { firestore, firestoreDb } from '../test/firebase-stubs';
 
 const test = fbTest();
 
 jest.spyOn(admin, 'initializeApp');
+jest.spyOn(admin, 'firestore' as any, 'get').mockReturnValue(() => firestore);
 
 describe('https authenticated callable', () => {
   beforeEach(() => {
@@ -23,6 +26,62 @@ describe('https authenticated callable', () => {
   it('should call handler on authenticated', async () => {
     const handler = jest.fn().mockReturnValue('result');
     const func = test.wrap(https.onAuthenticatedCall(handler));
+    await expect(func({}, { auth: { uid: 'test' } })).resolves.toBe('result');
+    expect(handler).toHaveBeenCalled();
+  });
+});
+
+describe('https authorized callable', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    firestore.clear();
+  });
+
+  it('should error on unauthenticated', async () => {
+    const handler = jest.fn();
+    const func = test.wrap(https.onAuthorizedCall(anyOf('admin'))(handler));
+    await expect(func({}, { auth: null })).rejects.toThrowError(
+      /authenticated/
+    );
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('should error on unauthorized: no user data', async () => {
+    const handler = jest.fn();
+    const func = test.wrap(https.onAuthorizedCall(anyOf('admin'))(handler));
+    await expect(func({}, { auth: { uid: 'test' } })).rejects.toThrowError(
+      /permission-denied/
+    );
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('should error on unauthorized: no roles', async () => {
+    const handler = jest.fn();
+    firestoreDb['auth/test'] = {};
+    const func = test.wrap(https.onAuthorizedCall(anyOf('admin'))(handler));
+    await expect(func({}, { auth: { uid: 'test' } })).rejects.toThrowError(
+      /permission-denied/
+    );
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('should error on unauthorized: missing roles', async () => {
+    const handler = jest.fn();
+    firestoreDb['auth/test'] = { roles: ['other_role'] };
+    const func = test.wrap(https.onAuthorizedCall(anyOf('admin'))(handler));
+    await expect(func({}, { auth: { uid: 'test' } })).rejects.toThrowError(
+      /permission-denied/
+    );
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('should call handler on authorized', async () => {
+    const handler = jest.fn().mockReturnValue('result');
+    firestoreDb['auth/test'] = { roles: ['admin'] };
+    const func = test.wrap(https.onAuthorizedCall(anyOf('admin'))(handler));
     await expect(func({}, { auth: { uid: 'test' } })).resolves.toBe('result');
     expect(handler).toHaveBeenCalled();
   });
--- functions/src/https.ts
+++ functions/src/https.ts
@@ -1,15 +1,16 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 export type AuthenticatedCallableContext = Required<
   functions.https.CallableContext
 >;
 
-export function onAuthenticatedCall(
-  handler: (
-    data: any,
-    context: AuthenticatedCallableContext
-  ) => any | Promise<any>
-) {
+type AuthenticatedHandler = (
+  data: any,
+  context: AuthenticatedCallableContext
+) => any | Promise<any>;
+
+export function onAuthenticatedCall(handler: AuthenticatedHandler) {
   return functions.https.onCall(async (data, context) => {
     if (!context.auth) {
       throw new functions.https.HttpsError(
@@ -17,6 +18,39 @@ export function onAuthenticatedCall(
         'must be authenticated'
       );
     }
-    return await handler(data, context as any);
+    return await handler(data, context as AuthenticatedCallableContext);
   });
 }
+
+export const anyOf = (...required: string[]) => (userRoles: string[]) => {
+  return required.some((r) => userRoles.includes(r));
+};
+
+export const allOf = (...required: string[]) => (userRoles: string[]) => {
+  return required.every((r) => userRoles.includes(r));
+};
+
+export const onAuthorizedCall = (assertion: (roles: string[]) => boolean) => (
+  handler: AuthenticatedHandler
+) => {
+  return functions.https.onCall(async (data, context) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        'unauthenticated',
+        'must be authenticated'
+      );
+    }
+    const userDoc = await admin
+      .firestore()
+      .collection('auth')
+      .doc(context.auth.uid)
+      .get();
+    if (!userDoc.exists || !assertion(userDoc.data()!.roles || [])) {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        'permission-denied'
+      );
+    }
+    return await handler(data, context as AuthenticatedCallableContext);
+  });
+};
